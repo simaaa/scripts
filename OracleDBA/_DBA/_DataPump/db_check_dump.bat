c322e963e39f5e80952d967b45cb208bb8ef6e4b
eLLZé <sima.hu@gmail.com>
2020-05-31 15:25:11 +0200

    Update db_check_dump.bat

--- OracleDBA/_DBA/_DataPump/db_check_dump.bat
+++ OracleDBA/_DBA/_DataPump/db_check_dump.bat
@@ -1,5 +1,5 @@
 @Echo Off
-sqlplus -s sys/sysmanager2020@DEV_UJ as sysdba @db_check_dump.pdc > db_check_dump.log
+sqlplus -s sys/sysmanager2020@DEV as sysdba @db_check_dump.pdc > db_check_dump.log
 SET SQL_RESULT=%ERRORLEVEL%
 ECHO RESULT=%SQL_RESULT%
 PAUSE
